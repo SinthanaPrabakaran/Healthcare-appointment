856670704239cd3e7d9338c3fa6f27fc76c3d4a4
SinthanaPrabakaran <Sinthana.2023@vitstudent.ac.in>
2026-08-24 14:52:25 +0530

    bcrypt password hashing before saving

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcryptjs from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,16 @@ const userSchema = new mongoose.Schema({
   }
 }, {
   timestamps: true
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
