8e2b8b3a4639353815faf6c2ff0c7be7f82fe8db
SinthanaPrabakaran <Sinthana.2023@vitstudent.ac.in>
2026-08-24 14:48:18 +0530

    basic user model with mongoose schema

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    default: 'PATIENT'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
